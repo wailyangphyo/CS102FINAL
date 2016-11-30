b316bcba52c888798a9df27c22a812f6a5a76a27
abinderclip <anna.d.shao@gmail.com>
2016-11-30 11:09:43 -0800

    Create findCorrectWindow.cpp

---
+++ findCorrectWindow.cpp
@@ -0,0 +1,23 @@
+#include "lab.h"
+
+std::string findCorrectWindow(Fl_button*, void*)
+{
+    std::cout<< wi->value() << hi->value() << endl' 
+    searchAPI(wi,hi)
+    
+    //how to call function??
+    if (b.status == "Underweight"){
+	makeUnderweightWindow()->show();
+    } else if (b.status == "Normal (healthy weight)"){
+	makeHealthyWindow()->show();
+    } else if (b.status == "Overweight"){
+	makeOverweightWindow()->show();
+    } else if (b.status == "Obese Class I (Moderately obese)"){
+	makeObeseWindow()->show();
+    } else if (b.status == "Obese Class II (Severely obese)"){
+	makeObeseWindow()->show();
+    } else if (b.status == "Obese Class III (Very severely obese)"){
+	makeSeverelyObeseWindow()->show();
+    } 
+	
+}
