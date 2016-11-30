c8f85e85bc0104b734f697dc4f901ac847756bc0
abinderclip <anna.d.shao@gmail.com>
2016-11-29 18:56:00 -0800

    Update makeInputWindow.cpp

--- makeInputWindow.cpp
+++ makeInputWindow.cpp
@@ -1,20 +1,23 @@
 #include "lab.h"
 Fl_Cairo_Window * cw;
-Fl_Input * in;
-Fl_Input * lb;
+Fl_Input * hi; //height in 
+Fl_Input * wi; //weight in
 Fl_Button * b;
+Fl_Box * ii;
 Fl_Cairo_Window* makeSearchWindow()
 {
-    cw = new Fl_Cairo_Window(width,height);
+    cw = new Fl_Cairo_Window(width,he);
     cw->label("BMI Calculator");
     cw->color(FL_GREEN);
-    in = new Fl_Input(.7*width,.25*height,.25*width,.1*height, 
+    hi = new Fl_Input(.7*width,.25*height,.25*width,.1*height, 
 			   "Height in Inches");
-    lb = new Fl_Input(.7*width,.5*height,.25*width,.1*height, 
+    wi = new Fl_Input(.7*width,.5*height,.25*width,.1*height, 
 			   "Weight in Pounds");
     b = new Fl_Button(.57*width,.75*height,.25*width,.1*height, 
 			   "Calculate");
-    b->callback(( Fl_Callback * ) cbGetInfo);
-    b->color(FL_WHITE);
+    //b->callback(( Fl_Callback * ) cbGetInfo);
+    //b->color(FL_WHITE);
+    ii = new Fl_Box(.05*width,.05*height,128,128);
+    ii->image(new Fl_PNG_Image("input.png"));
     return cw;
 }
