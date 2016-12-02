52a573702c6735067f2334bcb6117ae27c899af4
abinderclip <anna.d.shao@gmail.com>
2016-12-01 19:30:41 -0800

    Update cbHealthyWindow.cpp

--- cbHealthyWindow.cpp
+++ cbHealthyWindow.cpp
@@ -5,7 +5,7 @@ Fl_Cairo_Window* cbHealthyWindow(int w,int h)
 {
     dw = new Fl_Cairo_Window(w,h); 
     dw->label("Healthy BMI");
-    gb = new Fl_Box(0,200,512,384);
+    gb = new Fl_Box(0,150,512,384);
     gb ->image(new Fl_JPG_Image("healthyweight.jpg"));
     return dw;
 }
