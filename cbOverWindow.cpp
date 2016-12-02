ccda7533b1cbb9e1fa9b8dd3ae72352d682fca42
abinderclip <anna.d.shao@gmail.com>
2016-12-01 19:24:06 -0800

    Update cbOverWindow.cpp

--- cbOverWindow.cpp
+++ cbOverWindow.cpp
@@ -7,7 +7,7 @@ Fl_Cairo_Window* cbOverWindow(int w,int h)
     std::cout << "h: " << h << std::endl;
     dw = new Fl_Cairo_Window(w,h); 
     dw->label("Overweight BMI");
-    gb = new Fl_Box(0,200,512,600);
+    gb = new Fl_Box(0,200,512,384);
     gb ->image(new Fl_JPG_Image("overweight.jpg"));
     return dw;
 }
