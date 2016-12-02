def338b8fa31161940da98c18002dde645e2153f
abinderclip <anna.d.shao@gmail.com>
2016-12-01 19:29:30 -0800

    Update cbUnderWindow.cpp

--- cbUnderWindow.cpp
+++ cbUnderWindow.cpp
@@ -3,11 +3,9 @@ Fl_Cairo_Window * dw;
 Fl_Box * gb;
 Fl_Cairo_Window* cbUnderWindow(int w,int h)
 {
-    std::cout << "w: " << w << std::endl;
-    std::cout << "h: " << h << std::endl;
     dw = new Fl_Cairo_Window(w,h); 
     dw->label("Underweight BMI");
-    gb = new Fl_Box(0,200,512,600);
+    gb = new Fl_Box(0,150,512,384);
     gb ->image(new Fl_JPG_Image("underweight.jpg"));
     return dw;
 }
