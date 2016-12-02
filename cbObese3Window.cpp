58e73019ba354bea0685a1e4960e4334312c2314
abinderclip <anna.d.shao@gmail.com>
2016-12-01 19:30:02 -0800

    Update cbObese3Window.cpp

--- cbObese3Window.cpp
+++ cbObese3Window.cpp
@@ -5,7 +5,7 @@ Fl_Cairo_Window* cbObese3Window(int w,int h)
 {
     dw = new Fl_Cairo_Window(w,h); 
     dw->label("Morbidly Obese");
-    gb = new Fl_Box(0,200,512,384);
+    gb = new Fl_Box(0,150,512,384);
     gb ->image(new Fl_JPG_Image("morbidlyobese.jpg"));
     return dw;
 }
